cae22f656c4db0b10b6988f5c436b9cb4d8f24d4
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2025-07-01 22:41:55 +0500

    Add initial set of files

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hproject;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
